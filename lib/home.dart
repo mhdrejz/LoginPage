ac3783d29e9063e8f0e0289102a422df59c7e25d
mhdrejz <muhammedrajnaz@gmail.com>
2022-09-23 23:43:52 +0400

    New Changes

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('RejzApp'),
+        title: const Text('RejzAp'),
         actions: [
           IconButton(
               onPressed: () {
